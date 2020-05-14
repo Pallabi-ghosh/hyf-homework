873335a8c98eae985fefa7cda9f6189982404d84
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-05-14 11:23:58 +0200

    I have calculated my age here

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,6 @@
+//trying to calculate my age//
+let yearOfBirth = 1993;
+let yearFuture = 2045;
+let age = yearFuture - yearOfBirth;
+
+console.log(" You will be ".concat(age).concat(" years old in 2045 "));
