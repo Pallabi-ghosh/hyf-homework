0ba84023446ec8b6b668dd945c4fda173692d932
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-06-01 11:55:49 +0200

    change the function structure and function call after decelaration

--- javascript/javascript1/week3/save_notes.js
+++ javascript/javascript1/week3/save_notes.js
@@ -1,26 +1,26 @@
 const notes = [];
 
+function saveNotes(content, id) {
+    notes.push({ content: content, id: id });
+}
 saveNotes('shopping facewash', 1);
 saveNotes('pick up something', 2);
 saveNotes('pick up a dress', 3);
 saveNotes('food items', 4);
 
-
-function saveNotes(k, j) {
-    var myObject = {
-        content: k,
-        id: j
-    }
-    notes.push(myObject)
-}
 console.log(notes);
 
 function getNote(id) {
-    var i = id - 1;
-    console.log(notes[i])
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === id) {
+            return notes[i];
+        }
+    }
 }
 
 const firstNote = getNote(4);
+
+console.log(firstNote)
 
 function logOutNotesFormatted(k, j) {
     for (i = 0; i < notes.length; i++)
