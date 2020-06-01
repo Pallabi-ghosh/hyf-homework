d96baf8a0019c5f0c2ec7ca40df765b1695435f3
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-06-01 11:54:47 +0200

    change the function structure and function call after decelaration

--- javascript/javascript1/week3/CactusIO_interactive.js
+++ javascript/javascript1/week3/CactusIO_interactive.js
@@ -1,16 +1,13 @@
-const activity = [];
+const activities = [];
+
+function addActivity(date, activity, duration) {
+    activities.push({ date, activity, duration });
+}
+
 
 addActivity('23/7-18', 'Youtube', 30);
 addActivity('23/7-18', 'Telivision', 15);
 addActivity('23/7-18', 'Instagram', 60);
 addActivity('23/7-18', 'Facebook', 19);
 
-function addActivity(d, a, dura) {
-    var myObject = {
-        date: d,
-        activity: a,
-        duration: dura
-    }
-    activity.push(myObject)
-}
-console.log(activity);
+console.log(activities);
