05776ad0335deffaa400b96a8baa1f57e6e96635
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-06-01 11:53:13 +0200

    const insted of var and function call after decleration

--- javascript/javascript1/week3/travelInformation.js
+++ javascript/javascript1/week3/travelInformation.js
@@ -3,17 +3,15 @@ const travelInformation = {
     destinationDistance: 432,
 };
 
-const travelTime = travelFunction(travelInformation);
-
 function travelFunction(travelInformation) {
-    var speedInMinute = travelInformation["speed"] / 60
-    var time = Math.floor(travelInformation["destinationDistance"] / speedInMinute)
-    var minutes = time % 60
-    var totalHour = (time - minutes) / 60
+    const speedInMinute = travelInformation["speed"] / 60
+    const time = Math.floor(travelInformation["destinationDistance"] / speedInMinute)
+    const minutes = time % 60
+    const totalHour = (time - minutes) / 60
     return (totalHour + " hours and " + minutes + " minutes");
 
 
 }
-
+const travelTime = travelFunction(travelInformation);
 
 console.log(travelTime); // 8 hours and 64 minutes
