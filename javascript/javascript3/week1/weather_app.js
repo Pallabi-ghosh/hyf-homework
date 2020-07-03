2500f4c868ec4983af3caecd51f98bc957c5e6a7
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-07-03 13:57:02 +0200

    I have made this weather_app

---
+++ javascript/javascript3/week1/weather_app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var loc;
+
+var element = document.createElement("button");
+element.appendChild(document.createTextNode("Click here"));
+var page = document.getElementById("btn");
+page.appendChild(element);
+page.addEventListener("click", onload, false);
+
+function onload() {
+    loc = document.getElementById("recomendationInput");
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + loc.value + "&APPID=a56d332f49d2d3dfdd3f3443f04684f2")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+}
