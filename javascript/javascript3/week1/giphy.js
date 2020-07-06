504278bc75e742898dfd35143110a713d501be4b
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-07-06 09:49:32 +0200

    I have made this weather_app.html and giphy.js

--- javascript/javascript3/week1/giphy.js
+++ javascript/javascript3/week1/giphy.js
@@ -1,4 +1,4 @@
-var element = document.createElement("button");
+const element = document.createElement("button");
 element.appendChild(document.createTextNode("Click here"));
 var page = document.getElementById("btn");
 page.appendChild(element);
@@ -8,8 +8,8 @@ const searchButton = document.querySelector(".btn")
 
 
 function onload() {
-    loc = document.getElementById("recomendationInput");
-    fetch("https://api.giphy.com/v1/gifs/search?q=" + loc.value + "&api_key=dc6zaTOxFJmzC")
+    const location = document.getElementById("recomendationInput");
+    fetch("https://api.giphy.com/v1/gifs/search?q=" + location.value + "&api_key=dc6zaTOxFJmzC")
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
@@ -18,9 +18,7 @@ function onload() {
 
 
 function gotData(giphy) {
-    for (var i = 0; i < giphy.data.length; i++) {
-        createImg(giphy.data[i].images.original.url);
-    }
+
     forEach(imagesPreview => {
         const createImageTag = document.createElement("img")
         createImageTag.setAttribute("src", imagesPreview);
