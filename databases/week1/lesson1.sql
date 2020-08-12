cc52cd8d41dadebc728e9a69629eca52bc595cf4
Pallabi Ghosh Das <pallabighoshdas@pallabis-macbook-pro.home>
2020-08-12 11:05:46 +0200

    I have made the changes in lesson1.sql after got the feedback

--- databases/week1/lesson1.sql
+++ databases/week1/lesson1.sql
@@ -7,14 +7,42 @@ SELECT * FROM task
 WHERE status_id = 3;
 SELECT * FROM status;
 SELECT * FROM task
-WHERE status_id IS NULL;
+WHERE status_id IS NOT;
 -- Get the title and due date of all tasks where the title or description contains database --
-SELECT * FROM task
-WHERE description or title LIKE '%database%';
+SELECT 
+    id,
+    title, 
+    description,
+    due_date 
+FROM task 
+WHERE title LIKE '%database%' OR description LIKE '%database%';
 -- Get the title and status (as text) of all tasks --
-SELECT task.title,status.name FROM task JOIN status ON task.status_id=status.id;
+SELECT 
+	status.id, 
+    task.title, 
+    status.name 
+FROM task JOIN status ON task.status_id = status.id;
 -- Get the name of each status, along with a count of how many tasks have that status --
-select task.*, count(title) as status_id
-from task
-  left join title on gists.id = stars.gist_id;
+SELECT 
+	status.name, 
+    COUNT(status.name) 
+AS 
+	count 
+FROM 
+	status 
+JOIN task ON status.id = task.status_id GROUP BY status.name;
+
+-- Get the names of all statuses, sorted by the status with most tasks first
+SELECT 
+	status.name, 
+    COUNT(status.name) 
+AS 
+	count
+FROM 
+	status 
+JOIN 
+	task  
+ON 
+	status.id = task.status_id 
+GROUP BY status.name ORDER BY count DESC;
 
