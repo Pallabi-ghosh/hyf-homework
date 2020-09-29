7c3912b4e78cbcf0ee66f5db4106cc11074e215e
Pallabi Ghosh Das <pallabighosh3100@gmail.com>
2020-09-29 13:02:06 +0200

    feedback implimented nodejs week3

--- nodejs/week3/homework/Warmup/Circle_calculator.js
+++ nodejs/week3/homework/Warmup/Circle_calculator.js
@@ -1,27 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 var PORT = 3002;
 
-
-
 function Circle(radius) {
-    this.radius = radius;
-    // getArea method
-    this.area = function() {
-        return Math.PI * this.radius * this.radius;
-    };
-    // getDiameter method
-    this.getDiameter = function() {
-        return 2 * Math.PI * this.radius;
-    };
-
+  this.radius = radius;
+  // getArea method
+  this.area = function () {
+    return Math.PI * this.radius * this.radius;
+  };
+  // getDiameter method
+  this.getDiameter = function () {
+    return 2 * Math.PI * this.radius;
+  };
 }
-var c = new circle(3);
-var circumferenceOfCircle = 2 * Math.PI * ('Area =', c.area().toFixed(2));
-console.log('Area =', c.area().toFixed(2));
-console.log('Diameter =', c.getDiameter().toFixed(2));
-console.log("Circumference of circle is: " + circumferenceOfCircle);
 
+const c = new Circle(3);
+var circumferenceOfCircle = 2 * Math.PI * ("Area =", c.area().toFixed(2));
+console.log("Area =", c.area().toFixed(2));
+console.log("Diameter =", c.getDiameter().toFixed(2));
+console.log("Circumference of circle is: " + circumferenceOfCircle);
 
 var operator = req.params.operator;
 var num1 = parseInt(req.query.num1);
@@ -29,52 +26,48 @@ var num2 = parseInt(req.query.num2);
 var result;
 
 switch (operator) {
-    case "addition":
-        result = add(num1, num2);
-        break;
+  case "addition":
+    result = add(num1, num2);
+    break;
 
-    case "subtraction":
-        result = subtract(num1, num2);
-        break;
+  case "subtraction":
+    result = subtract(num1, num2);
+    break;
 
-    case "multiplication":
-        result = multiply(num1, num2);
-        break;
+  case "multiplication":
+    result = multiply(num1, num2);
+    break;
 
-    case "division":
-        result = divide(num1, num2);
-        break;
+  case "division":
+    result = divide(num1, num2);
+    break;
 
-    default:
-        result = "Sorry, please enter a valid operator!"
+  default:
+    result = "Sorry, please enter a valid operator!";
 }
 
 function add(a, b) {
-    return a + b
-
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b
+  return a / b;
 }
 
-
-
-app.get("/", function(request, respond) {
-    respond.send("hello! please type some math y'all")
-    request.json(result);
+app.get("/", function (request, respond) {
+  respond.send("hello! please type some math y'all");
+  request.json(result);
 });
 
-
 //listener
-app.listen(PORT, function() {
-    console.log("Example app listening on port 3002!");
+app.listen(PORT, function () {
+  console.log("Example app listening on port 3002!");
 });
