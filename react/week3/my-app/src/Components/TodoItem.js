79697bd9e712fe753224bc4867d9bc462f560380
Pallabi Ghosh Das <pallabighosh3100@gmail.com>
2020-10-28 15:15:38 +0100

    made the changes needed

--- react/week3/my-app/src/Components/TodoItem.js
+++ react/week3/my-app/src/Components/TodoItem.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.onMarkItemComplete = (event) => {
       this.props.markItemComplete(this.props.id);
